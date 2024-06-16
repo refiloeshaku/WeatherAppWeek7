e1f6a43a8914c2f01430808a247912140eb6bd8d
Refiloe Shaku <refiloeshaku2@gmail.com>
2024-06-16 17:54:51 +0000

    added a API key

--- src/index.js
+++ src/index.js
@@ -40,7 +40,7 @@ function formatDate(date) {
 }
 
 function searchCity(city) {
-  let apiKey = "b2a5adcct04b33178913oc335f405433";
+  let apiKey = "210ba7t1a0cb8b6ffod03b3abe8461da";
   let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
   axios.get(apiUrl).then(refreshWeather);
 }
